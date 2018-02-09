f01cb3fc23c0643c5a5bf2fbac01b91283d5ded3
Carolyn Larocque <clarocque1@live.ca>
2018-02-09 11:29:22 -0500

    working with ejs

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const templateTypes = [
+  { name: "Jade", usefulness : "sucky" },
+  { name: "EJS", usefulness : "a little better" },
+  { name: "Handlebars", usefulness : "awesome" }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'Express',
-    description: "sucks!!",
-    method: "easy!"
+    title: 'EJS',
+    descriptor: "better!",
+    templates: templateTypes
   });
 });
 
